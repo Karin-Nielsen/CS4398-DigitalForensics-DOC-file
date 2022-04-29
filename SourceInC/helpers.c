817a248d9bbe60c0e0bb7e909d5313d4b6207587
Karin Nielsen <74790359+Karin-Nielsen@users.noreply.github.com>
2022-04-28 21:08:55 -0500

    Create helpers.c with readClusters() function

---
+++ SourceInC/helpers.c
@@ -0,0 +1,20 @@
+/*
+    Filename: helpers.c
+    Author: Karin Nielsen
+    Date: 04/25/2022
+    Description: 
+        
+        Contains functions: 
+          - readClusters(): 
+              * Read one cluster into a buffer (4096 bytes).
+              
+          - xxx
+              *xxx
+            
+*/
+
+
+void readClusters(int fd) {
+
+
+}
